fa8a4782c594cb3a2bb89bb30320695d4b65c871
JohanAcosta19 <98934936+JohanAcosta19@users.noreply.github.com>
2023-09-19 22:34:05 -0600

    Update Factorial.js

--- Factorial.js
+++ Factorial.js
@@ -1,8 +1,8 @@
-function factorialRecursivo (n) { 
+function factorial (n) { 
 	if (n == 0){ 
 		return 1; 
 	}
-	return n * factorialRecursivo (n-1); 
+	return n * factorial (n-1); 
 }
 var n=10
-console.log("El factorial es : " + factorialRecursivo);
+console.log("El factorial es : " + factorial);
